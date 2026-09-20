7a68292ead60f22d2083ed84f6a4de07e00b6b65
manoj19-03 <manoj.h@wipro.com>
2026-09-20 19:00:37 +0530

    demo done

--- main.tf
+++ main.tf
@@ -8,10 +8,34 @@ terraform {
   }
 }
 
+resource "aws_security_group" "ec2_sg" {
+  name        = "terraform-ec2-sg"
+  description = "Allow SSH access to EC2"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "my_ec2" {
   ami           = var.ami_id
   instance_type = var.instance_type
   key_name      = var.key_name
+
+  security_groups = [
+    aws_security_group.ec2_sg.name
+  ]
 
   tags = {
     Name = var.instance_name
--- output.tf
+++ output.tf
@@ -7,3 +7,8 @@ output "instance_public_ip" {
   description = "EC2 public IP address"
   value       = aws_instance.my_ec2.public_ip
 }
+
+output "ssh_command" {
+  description = "SSH command to connect to the EC2 instance"
+  value       = "ssh -i ~/downloads/jala.pem.pem ubuntu@${aws_instance.my_ec2.public_ip}"
+}
